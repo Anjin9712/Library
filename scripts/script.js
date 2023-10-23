6fd8811f5591303fce090140bb5f6f5b7dbfd329
Kvothe <hoyosf250@gmail.com>
2023-10-23 05:49:29 -0500

    - Add new book feature
    - Add popup feature for new book cards and for new book form

--- scripts/script.js
+++ scripts/script.js
@@ -5,7 +5,7 @@ function Book(title, author, year, status, image_path) {
     this.author = author;
     this.year = year;
     this.status = status;
-    this.image = image_path
+    this.image_path = image_path
 }
 
 function addBookToHtml(title, author, year, status, image_path) {
@@ -13,57 +13,117 @@ function addBookToHtml(title, author, year, status, image_path) {
     /* sets book card */
 
     const bookCard = document.createElement("div");
-    const bookCover = document.createElement("img");
+    const bookCover1 = document.createElement("img");
     const bookTitle = document.createElement("h3");
     bookCard.classList.add("book");
-    bookCover.src = image_path;
+    bookCover1.src = image_path;
     bookTitle.textContent = title;
-    bookCard.appendChild(bookCover);
+
+    bookCard.appendChild(bookCover1);
     bookCard.appendChild(bookTitle);
 
-    /* sets dialog for book card*/
+    /* sets modal dialog for book card */
 
+    const modalDialog = document.createElement('dialog');
+    modalDialog.classList.add("bookModal");
+
+    const infoDiv = document.createElement("div");
+    infoDiv.classList.add("info");
+
+    const bookCover2 = document.createElement("img");
+    bookCover2.src = image_path;
+
+    /* Title */
+
+    const p1 = document.createElement("p");
+    const span1 = document.createElement("span");
+    p1.appendChild(span1);
+    span1.textContent = "Title:  ";
+    p1.append(title);
+    
+    /* Author */
+
+    const p2 = document.createElement("p");
+    const span2 = document.createElement("span");
+    span2.textContent = "Author:  ";
+    p2.appendChild(span2);
+    p2.append(author);
+
+    /* Year */
+
+    const p3 = document.createElement("p");
+    const span3 = document.createElement("span");
+    span3.textContent = "Year:  ";
+    p3.appendChild(span3);
+    p3.append(String(year));
+
+    /* Status */
+
+    const statusDiv =  document.createElement("div");
+    statusDiv.setAttribute("id", "divStatus")
+
+    const p4 = document.createElement("p");
+    const span4 = document.createElement("span");
+    p4.appendChild(span4);
+    span4.textContent = "Status:  ";
+    p4.append(String(status));
+
+    const changeBtn = document.createElement("button");
+    changeBtn.textContent = "Change Status";
+
+    statusDiv.append(p4, changeBtn);
+    infoDiv.append(p1, p2, p3, statusDiv);
+    modalDialog.append(bookCover2, infoDiv);
+
+    bookCard.appendChild(modalDialog);
+
+
+    /* Adds card to DOM */
+
+    const bookList = document.querySelector(".book-list");
+    bookList.appendChild(bookCard);
+
+    /* Adds event listener */
+
+    bookCard.addEventListener("click", (event) => {
+        modalDialog.showModal()
+    })
+    
+}
+
+function addBookToArray(title, author, year, status, image_path) {
+    library.push(new Book(title, author, year, status, image_path));
 }
 
 function addBookToLibrary(title, author, year, status, image_path) {
 
     /* adds book to array and html */
 
-    library.push(new Book(title, author, year, status, image_path));
+    addBookToArray(title, author, year, status, image_path);
     addBookToHtml(title, author, year, status, image_path);
 }
 
-/* events to open and close addModal dialog */
+const title = document.querySelector("#title");
+const author = document.querySelector("#author");
+const year = document.querySelector('#year');
+const readStatus = document.querySelector("#status");
+const image_path = document.querySelector("#coverImagePath");
 
-/*
-const addCardContainer = document.querySelector(".add-container");
-const addBookModal = document.querySelector(".addModal");
-addCardContainer.addEventListener('click', (event) => {
-    addBookModal.showModal();
+const addModal = document.querySelector(".addModal");
+const addButton = document.querySelector("#addButton");
+
+const addCard = document.querySelector("#addCard")
+
+/* event listener to open dialog to add new book */
+
+addCard.addEventListener("click", (event) => {
+    addModal.showModal()
 })
-*/
 
 /* event listener to add new book (to both array & html) */
 
-/*const addBtn = document.querySelector("#add-button");
-addBtn.addEventListener("click", (event) => {
-    const title = document.querySelector("#title").value;
-    const author = document.querySelector("#author").value;
-    const year = document.querySelector("#year").value;
-    const status = document.querySelector("#status").value;
-    const image = document.querySelector("#image").value;
-
-    addBookToLibrary(title, author, year, status, image)
-    console.log(library)
+addButton.addEventListener("click", (event) => {
+    addBookToLibrary(title.value, author.value, year.value, readStatus.value, image_path.value);
+    addModal.close();
 })
 
-/* event listener to open and close bookModal dialog */
-
-const books = document.querySelectorAll(".book");
-
-books.forEach(book => {
-    book.addEventListener("click", (event) => {
-        const bookModal = book.querySelector(".bookModal");
-        bookModal.showModal()
-    })
-})
